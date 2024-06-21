79c5297159a10a030da6d204564de6aeec9d5906
thigoap <appthigo@gmail.com>
2024-06-21 01:06:07 -0300

    Added route GET users/{id} with tests.

--- tests/test_app.py
+++ tests/test_app.py
@@ -81,6 +81,22 @@ def test_read_users_with_users(client, user):
     assert response.json() == {'users': [user_schema]}
 
 
+def test_read_user(client, user):
+    response = client.get('/users/1')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'Teste',
+        'email': 'teste@test.com',
+        'id': 1,
+    }
+
+
+def test_read_user_not_found_404(client, user):
+    response = client.get('/users/2')
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
+
+
 def test_update_user(client, user):
     response = client.put(
         '/users/1',
@@ -117,7 +133,7 @@ def test_delete_user(client, user):
     assert response.json() == {'message': 'User deleted'}
 
 
-def test_delete_user_not_found_400(client, user):
+def test_delete_user_not_found_404(client, user):
     response = client.delete('/users/2')
     assert response.status_code == HTTPStatus.NOT_FOUND
     assert response.json() == {'detail': 'User not found'}
